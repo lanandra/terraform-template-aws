243cc0acb11b6f73f3a16b6f8c0792c21a9fa355
lanandra <luthfi.anandra@gmail.com>
2022-01-15 17:11:14 +0700

    Update all EC2 to use data reference for subnet and sec-group

--- ec2/ec2-apse1/ec2-public.tf
+++ ec2/ec2-apse1/ec2-public.tf
@@ -20,8 +20,8 @@ module "demo_ubuntu_public_ip_disabled" {
   count  = 1
 
   associate_public_ip = false
-  vpc_security_group  = var.vpc_security_group_public
-  vpc_subnet          = var.vpc_subnet_public[0]
+  vpc_security_group  = data.terraform_remote_state.vpc_ap_southeast_1.outputs.sec_group_demo_public_apse1_0
+  vpc_subnet          = data.terraform_remote_state.vpc_ap_southeast_1.outputs.subnet_public_apse1_0
 
   ec2_tags = {
     Name = "demo-ubuntu-public-${count.index}"
@@ -34,8 +34,8 @@ module "demo_ubuntu_arm_public_ip_enabled" {
   count  = 1
 
   associate_public_ip = true
-  vpc_security_group  = var.vpc_security_group_public
-  vpc_subnet          = var.vpc_subnet_public
+  vpc_security_group  = data.terraform_remote_state.vpc_ap_southeast_1.outputs.sec_group_demo_public_apse1_0
+  vpc_subnet          = data.terraform_remote_state.vpc_ap_southeast_1.outputs.subnet_public_apse1_0
 
   ec2_tags = {
     Name = "demo-ubuntu-arm-${count.index}"
@@ -48,8 +48,8 @@ module "demo_amazon_linux_2_public_ip_enabled" {
   count  = 1
 
   associate_public_ip = true
-  vpc_security_group  = var.vpc_security_group_public
-  vpc_subnet          = var.vpc_subnet_public
+  vpc_security_group  = data.terraform_remote_state.vpc_ap_southeast_1.outputs.sec_group_demo_public_apse1_0
+  vpc_subnet          = data.terraform_remote_state.vpc_ap_southeast_1.outputs.subnet_public_apse1_0
 
   ec2_tags = {
     Name = "demo-amazon-linux-2-${count.index}"
